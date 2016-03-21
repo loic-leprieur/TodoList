1f30ffcc3c0b5d9eaf55703666e90f5c5b1113f7
donleprieur <donleprieur@gmail.com>
2016-03-21 23:03:38 +0100

    Ajout du floating button

--- app/src/main/java/fr/vincent_leprieur/todolist/MainActivity.java
+++ app/src/main/java/fr/vincent_leprieur/todolist/MainActivity.java
@@ -1,5 +1,13 @@
 package fr.vincent_leprieur.todolist;
-
+FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
